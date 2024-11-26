14e8482a6d04217947ad425e374837d498d1d05e
mark angelo <markpetpet0827@gmail.com>
2024-11-26 15:35:58 +0800

    added logout and dashboard for student

--- app/Http/Controllers/Student/DashboardController.php
+++ app/Http/Controllers/Student/DashboardController.php
@@ -4,11 +4,11 @@ namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
     public function index(){
-        return "hello";
         $totalusers = User::count();
 
         return view('student.dashboard.index',[
