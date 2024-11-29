0766f1d602cea01274a7252943edc4b3a43e6c5d
mark angelo <markpetpet0827@gmail.com>
2024-11-29 15:05:34 +0800

    modified href button on dashboard button

--- resources/views/layouts/student.blade.php
+++ resources/views/layouts/student.blade.php
@@ -36,7 +36,7 @@
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+                <div class="sidebar-brand-text mx-3">SB Student <sup>2</sup></div>
             </a>
 
             <!-- Divider -->
@@ -44,7 +44,8 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
-                <a class="nav-link" href="index.html">
+                
+                <a class="nav-link" href="">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
