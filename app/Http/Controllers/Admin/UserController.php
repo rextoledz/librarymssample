b8eda145fc1fb4d7a041b1dfec66ae96cac678d7
rexal <rextoldz26@gmail.com>
2024-12-02 09:43:10 +0800

    changed manage-user location

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function index() {
-        return view('admin.dashboard.manage-user', [
+        return view('admin.manage-user.index', [
         	'nav' => 2
         ]);
     }
